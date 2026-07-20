ff64f1c55c6106b6a36e160921e9487c77916500
alex-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2026-07-20 10:33:06 +0200

    [behaviour_library] pose_pub_behaviour -> object_pose_pub_behaviour
    The behaviour now takes an object as an input

--- behaviour_library/include/behaviour_library/object_pose_pub_behaviour.hpp
+++ behaviour_library/include/behaviour_library/object_pose_pub_behaviour.hpp
@@ -1,22 +1,23 @@
-#ifndef POSE_PUB_BEHAVIOUR_HPP
-#define POSE_PUB_BEHAVIOUR_HPP
+#ifndef OBJECT_POSE_PUB_BEHAVIOUR_HPP
+#define OBJECT_POSE_PUB_BEHAVIOUR_HPP
 
 #include <behaviortree_ros2/bt_topic_pub_node.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include "robot_swiss_knife_msgs/msg/object.hpp"
 
 using namespace BT;
 
 /**
- * A behaviour that publishes a PoseStamped message that is saved on the blackboard.
+ * A behaviour that publishes a PoseStamped message from an object saved on the blackboard.
  * Based on the samples in https://github.com/BehaviorTree/BehaviorTree.ROS2
  *
  * @author Alex Mitrevski
  * @contact alemitr@chalmers.se
  */
-class PosePubBehaviour : public RosTopicPubNode<geometry_msgs::msg::PoseStamped>
+class ObjectPosePubBehaviour : public RosTopicPubNode<geometry_msgs::msg::PoseStamped>
 {
 public:
-    PosePubBehaviour(const std::string& name, const NodeConfig& conf, const RosNodeParams& params);
+    ObjectPosePubBehaviour(const std::string& name, const NodeConfig& conf, const RosNodeParams& params);
 
     /**
      * Sets the ports of the behaviour:
