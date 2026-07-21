cf90fdc11b19f0137f3e8a475a85e5516905dc6d
alex-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2026-07-21 19:16:07 +0200

    [behaviour_library] Added a new behaviour that sends a named pose goal and waits for the result

---
+++ behaviour_library/include/behaviour_library/move_arm_to_named_pose_behaviour.hpp
@@ -0,0 +1,38 @@
+#ifndef MOVE_ARM_TO_NAMED_POSE_BEHAVIOUR_HPP
+#define MOVE_ARM_TO_NAMED_POSE_BEHAVIOUR_HPP
+
+#include <behaviortree_cpp/action_node.h>
+#include <behaviortree_ros2/ros_node_params.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/bool.hpp>
+
+using namespace std::placeholders;
+using namespace BT;
+
+/**
+ * A behaviour that publishes a String message from with the name of a predefined
+ * pose to which a robot's arm should move and waits for a result to be received.
+ *
+ * @author Alex Mitrevski
+ * @contact alemitr@chalmers.se
+ */
+class MoveArmToNamedPoseBehaviour : public BT::StatefulActionNode
+{
+public:
+    MoveArmToNamedPoseBehaviour(const std::string& name, const BT::NodeConfig& conf, const BT::RosNodeParams& params);
+    static PortsList providedPorts();
+    NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
+    void result_cb(std_msgs::msg::Bool::SharedPtr msg);
+private:
+    rclcpp::Node::SharedPtr node;
+    std::string goal_topic_name;
+    std::string result_topic_name;
+    std::string pose_name;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr goal_pub;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr result_sub;
+    std_msgs::msg::Bool::SharedPtr execution_result_msg;
+};
+
+#endif
