d851212055abe1f4f24602e052c85696ba5176de
alex-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2026-07-23 20:33:16 +0200

    [behaviour_library/move_arm_to_named_pose_behaviour] Added doctrings

--- behaviour_library/include/behaviour_library/move_arm_to_named_pose_behaviour.hpp
+++ behaviour_library/include/behaviour_library/move_arm_to_named_pose_behaviour.hpp
@@ -20,16 +20,56 @@ class MoveArmToNamedPoseBehaviour : public StatefulActionNode
 {
 public:
     MoveArmToNamedPoseBehaviour(const std::string& name, const NodeConfig& conf, const RosNodeParams& params);
+
+    /**
+     * Sets the ports of the behaviour:
+     * - Input ports:
+     *     * goal_topic_name (std::string)
+     *     * result_topic_name (std::string)
+     *     * pose_name (std::string)
+     */
     static PortsList providedPorts();
+
+    /**
+     * Sends an arm motion request and initialises a subscriber
+     * that waits for feedback. Clears any prior messages
+     * that have been received.
+     */
     NodeStatus onStart() override;
+
+    /**
+     * If an arm execution result message has been received, returns
+     * SUCCESS or FAILURE depending on the message contents.
+     * Returns RUNNING otherwise.
+     */
     NodeStatus onRunning() override;
+
+    /**
+     * Cleans up the component by stopping the execution result subscriber.
+     */
     void onHalted() override;
+
+    /**
+     * Stores the received message.
+     */
     void result_cb(std_msgs::msg::Bool::SharedPtr msg);
 private:
-    rclcpp::Node::SharedPtr node;
+    /**
+     * Topic on which named arm goals can be sent.
+     */
     std::string goal_topic_name;
+
+    /**
+     * Topic on which arm execution results are received.
+     */
     std::string result_topic_name;
+
+    /**
+     * Name of an arm pose to which a robot should be sent.
+     */
     std::string pose_name;
+
+    rclcpp::Node::SharedPtr node;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr goal_pub;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr result_sub;
     std_msgs::msg::Bool::SharedPtr execution_result_msg;
