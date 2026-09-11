7aeb389a9234f18567f8456417ac1cce9e16de62
alex-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2026-09-11 08:31:21 +0200

    [knowledge/kb_interface] Only consider fluents to be equal if the values are also equal

--- knowledge/kb_interface/kb_interface/knowledge_base_interface.py
+++ knowledge/kb_interface/kb_interface/knowledge_base_interface.py
@@ -363,6 +363,7 @@ class KnowledgeBaseInterface(object):
                 if param not in item['params']:
                     exists = False
                     break
+            exists = item['value'] == kb_item['value']
             if exists:
                 object_id = kb_item['_id']
                 break
